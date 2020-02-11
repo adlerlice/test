d26679a330f178b4f9ece5213972de98a4fada4d
alexandra k <adlerlice@gmail.com>
2020-02-11 14:57:03 +0700

    Добавьте файлы проекта.

---
+++ feb11/feb11.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <math.h>
+#include <fstream>
+
+
+using namespace std;
+int main()
+{
+	std::ifstream ifs("file.txt");
+
+
+	in.seekg(0, in.end);
+
+	int n = in.tellg();// размер файла
+
+	in.seekg(0);
+
+	char* text = new char[n];
+	for (int i = 0; i != n; i++) {
+		char s;
+		if (in.get(s)) {
+			text[i] = s;
+		}
+	}
+	cout << text;
+
+
+
+
+
+}
